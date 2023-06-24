4b6e79add4b7790eccd0cf234a11845460abb485
dev-rashidul <rashidul.contact@gmail.com>
2023-06-24 12:19:39 +0600

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,38 @@
 // JavaScript For Sticky Header
 
-window.addEventListener("scroll", function(){
-    const header = document.querySelector(".navbar");
-    header.classList.toggle("sticky", window.scrollY > 0)
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".navbar");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
-})
+// Counter JS
+
+var counted = 0;
+$(window).scroll(function () {
+  var oTop = $("#counter").offset().top - window.innerHeight;
+  if (counted == 0 && $(window).scrollTop() > oTop) {
+    $(".counter").each(function () {
+      var $this = $(this),
+        countTo = $this.attr("data-count");
+      $({
+        countNum: $this.text(),
+      }).animate(
+        {
+          countNum: countTo,
+        },
+
+        {
+          duration: 1000,
+          easing: "swing",
+          step: function () {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function () {
+            $this.text(this.countNum);
+          },
+        }
+      );
+    });
+    counted = 1;
+  }
+});
